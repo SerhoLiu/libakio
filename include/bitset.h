769f61899faeb5cb8938df274ebb37df3b613da0
Serho Liu <serholiu@gmail.com>
2013-11-24 17:09:38 +0800

    增加注释

--- include/bitset.h
+++ include/bitset.h
@@ -12,20 +12,48 @@
 
 typedef struct bitset bitset;
 
+/**
+ * 创建 length 个 bit 的 Bitset
+ */
 bitset *bitset_new(unsigned long length);
 
+/**
+ * 释放 Bitset
+ */
 void bitset_free(bitset *set);
 
+/**
+ * 得到第 n 位的值
+ *
+ * Return value: 0, 1
+ */
 int bitset_get(bitset *set, unsigned long n);
 
+/**
+ * 将第 n 位设置为 bit 值，只能是0或1,其它值不会对 bitset 做任何改变
+ *
+ * Return value: n 位原值
+ */
 int bitset_set(bitset *set, unsigned long n, int bit);
 
+/**
+ * 将全部位置为 1
+ */
 void bitset_setall(bitset *set);
 
+/**
+ * 将全部位清零
+ */
 void bitset_clearall(bitset *set);
 
+/**
+ * 返回 bitset 位长度
+ */
 unsigned long bitset_length(bitset *set);
 
+/**
+ * 返回 bitset 中为1的位数量
+ */
 unsigned long bitset_count(bitset *set);
 
 
