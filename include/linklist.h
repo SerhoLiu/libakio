23c717675bf288b3c4c6438f7c4d909a7c58aec9
Serho Liu <serholiu@gmail.com>
2014-02-10 15:44:39 +0800

    linklist update

--- include/linklist.h
+++ include/linklist.h
@@ -10,8 +10,37 @@
 #ifndef _LIBAKIO_LINKLIST_H_
 #define _LIBAKIO_LINKLIST_H_
 
+typedef void free_value(void *value);
+
 typedef struct list_node list_node;
 typedef struct linklist linklist;
+
+
+#define linklist_length(l) ((l)->len)
+
+#define linklist_head(l) ((l)->head)
+
+#define linklist_tail(l) ((l)->tail)
+
+#define linklist_prev_node(n) ((n)->prev)
+
+#define linklist_next_node(n) ((n)->next)
+
+#define linklist_node_value(n) ((n)->value)
+
+
+linklist *linklist_new(free_value *func);
+
+void linklist_free(linklist *list);
+
+linklist *linklist_add_head(linklist *list, void *value);
+
+linklist *linklist_add_tail(linklist *list, void *value);
+
+linklist *linklist_insert_node(linklist *list, list_node *node,
+                               void *value, int after);
+
+void linklist_delete_node(linklist *list, list_node *node);
 
 
 #endif 
--- src/linklist.c
+++ src/linklist.c
@@ -21,5 +21,153 @@ struct linklist {
 
     unsigned long len;
 
-    void (*free_value)(void *ptr);
+    free_value *free_func;
 };
+
+
+linklist *linklist_new(free_value *func)
+{
+    struct linklist *list;
+
+    list = malloc(sizeof(*list));
+    if (list == NULL) return NULL;
+
+    list->head = list->tail = NULL;
+    list->len = 0;
+    list->free_func = func;
+
+    return list;
+}
+
+
+void linklist_free(linklist *list)
+{
+    if (list == NULL) return;
+    
+    unsigned long len;
+    struct list_node *current, *next;
+
+    current = list->head;
+    len = list->len;
+    while(len--) {
+        next = current->next;
+        if (list->free_func) {
+            list->free_func(current->value);
+        }
+        free(current);
+        current = next;
+    }
+    free(list);
+}
+
+
+linklist *linklist_add_head(linklist *list, void *value)
+{
+    if (list == NULL) return NULL;
+    
+    struct list_node *node = malloc(sizeof(*node));
+    if (node == NULL) return NULL;
+
+    node->value = value;
+
+    if (list->len == 0) {
+        list->head = list->tail = node;
+        node->prev = node->next = NULL;
+    } else {
+        node->prev = NULL;
+        node->next = list->head;
+        list->head->prev = node;
+        list->head = node;
+    }
+
+    list->len++;
+
+    return list;
+}
+
+
+linklist *linklist_add_tail(linklist *list, void *value)
+{
+    if (list == NULL) return NULL;
+    
+    struct list_node *node = malloc(sizeof(*node));
+    if (node == NULL) return NULL;
+
+    node->value = value;
+
+    if (list->len == 0) {
+        list->head = list->tail = node;
+        node->prev = node->next = NULL;
+    } else {
+        node->prev = list->tail;
+        node->next = NULL;
+        list->tail->next = node;
+        list->tail = node;
+    }
+
+    list->len++;
+
+    return list;
+}
+
+
+linklist *linklist_insert_node(linklist *list, list_node *node,
+                               void *value, int after)
+{
+    
+    if (list == NULL || node == NULL) return NULL;
+    
+    struct list_node *new_node = malloc(sizeof(*new_node));
+    if (new_node == NULL) return NULL;
+
+    new_node->value = value;
+
+    if (after) {
+        new_node->prev = node;
+        new_node->next = node->next;
+        if (list->tail == node) {
+            list->tail = new_node;
+        }
+    } else {
+        new_node->next = node;
+        new_node->prev = node->prev;
+        if (list->head == node) {
+            list->head = new_node;
+        }
+    }
+
+    if (new_node->prev != NULL) {
+        new_node->prev->next = new_node;
+    }
+    if (new_node->next != NULL) {
+        new_node->next->prev = new_node;
+    }
+
+    list->len++;
+
+    return list;
+}
+
+
+void linklist_delete_node(linklist *list, list_node *node)
+{
+    if (node->prev) {
+        node->prev->next = node->next;
+    } else {
+        list->head = node->next;
+    }
+        
+    if (node->next) {
+        node->next->prev = node->prev;
+    } else {
+        list->tail = node->prev;
+    }
+    
+    if (list->free_func) {
+        list->free_func(node->value);
+    }
+
+    free(node);
+
+    list->len--;
+}
