1357333f455fb829809227e546c9b50d9f96e91c
Serho Liu <serholiu@gmail.com>
2015-02-03 20:38:32 +0800

    Add buffer type

---
+++ kznet/buffer.c
@@ -0,0 +1,235 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/uio.h>
+
+#include "buffer.h"
+
+
+struct buffer_pool {
+    int maxsize;
+    int allocs;
+    int size;
+
+    struct buffer head;
+    struct buffer *tail;
+};
+
+
+buffer_pool_t *buffer_pool_create(int maxsize)
+{
+    buffer_pool_t *pool;
+
+    pool = malloc(sizeof(*pool));
+    if (pool == NULL) {
+        return NULL;
+    }
+
+    pool->maxsize = maxsize < 32 ? 32 : maxsize;
+    pool->size = pool->allocs = 0;
+    pool->head.next = NULL;
+    pool->tail = &pool->head;
+
+    return pool;
+}
+
+void buffer_pool_delete(buffer_pool_t *pool)
+{
+    buffer_t *curr, *next;
+
+    assert(pool != NULL);
+
+    for (curr = pool->head.next; curr != NULL; ) {
+        next = curr->next;
+        free(curr);
+        curr = next;
+    }
+    free(pool);
+}
+
+static buffer_t *buffer_pool_peek(buffer_pool_t *pool)
+{
+    buffer_t *buf;
+
+    assert(pool != NULL);
+
+    if (pool->head.next != NULL) {
+        return pool->head.next;
+    }
+
+    buf = malloc(BUFFER_SIZE);
+    if (buf == NULL) return NULL;
+    
+    buf->next = NULL;
+    buf->start = (unsigned char *)buf + BUFFER_HSIZE;
+    buf->pos = buf->last = buf->start;
+    buf->end = (unsigned char *)buf + BUFFER_SIZE;
+
+    pool->head.next = pool->tail = buf;
+    pool->size++;
+    pool->allocs++;
+
+    return buf;
+}
+
+static inline void buffer_detach(buffer_pool_t *pool, buffer_t *buf)
+{
+    pool->head.next = buf->next;
+    buf->next = NULL;
+    if (pool->head.next == NULL) {
+        pool->tail = &pool->head;
+    }
+    pool->size--;
+}
+
+buffer_t *buffer_pool_get(buffer_pool_t *pool)
+{
+    buffer_t *buf;
+
+    assert(pool != NULL);
+
+    buf = buffer_pool_peek(pool);
+    if (buf == NULL) return NULL;
+
+    buffer_detach(pool, buf);
+
+    return buf;
+}
+
+int buffer_pool_put(buffer_pool_t *pool, buffer_t *buf)
+{
+    assert(pool != NULL);
+    assert(buf != NULL);
+
+    if (pool->allocs > pool->maxsize) {
+        free(buf);
+        pool->allocs--;
+        return 0;
+    }
+
+    buf->next = NULL;
+    buf->pos = buf->last = buf->start;
+
+    pool->tail->next = buf;
+    pool->tail = buf;
+    pool->size++;
+
+    return 0;
+}
+
+ssize_t buffer_chain_recv(buffer_chain_t *chain, int fd, int *err)
+{
+    size_t   wlen, tlen;
+    ssize_t  rv, total;
+    buffer_t *wbuf;
+    buffer_t *temp;
+    buffer_pool_t *pool;
+    struct iovec vec[2];
+
+    total = 0;
+    pool = chain->pool;
+
+    while (1) {
+
+        wbuf = chain->wbuf;
+        wlen = buffer_wsize(wbuf);
+        temp = buffer_pool_peek(pool);
+        tlen = buffer_wsize(temp);
+
+        vec[0].iov_len = wlen;
+        vec[0].iov_base = wbuf->last;
+        vec[1].iov_len = tlen;
+        vec[1].iov_base = temp->last;
+
+        rv = readv(fd, vec, 2);
+        if (rv <= 0) {
+            *err = errno;
+            return rv;
+        }
+
+        total += rv;
+
+        if (rv < wlen) {
+            wbuf->last += rv;
+            break;
+        }
+
+        wbuf->last += wlen;
+        buffer_detach(pool, temp);
+        temp->last = temp->pos + (rv - wlen);
+        chain->wbuf->next = temp;
+        chain->wbuf = temp;
+
+        if (rv != wlen + tlen) {
+            break;
+        }
+
+        temp = buffer_pool_get(pool);
+        chain->wbuf->next = temp;
+        chain->wbuf = temp;
+    }
+
+    return total;
+}
+
+ssize_t buffer_chain_send(buffer_chain_t *chain, int fd, int *err)
+{
+    int i;
+    size_t   rlen;
+    ssize_t  rv, total;
+    buffer_t *buf;
+    buffer_pool_t *pool;
+    struct iovec vec[SM_MAX_IOV];
+
+    total = 0;
+    pool = chain->pool;
+    
+    while (1) {
+
+        i = 0;
+        for (buf = chain->rbuf; buf != NULL; buf = buf->next) {
+            vec[i].iov_base = buf->pos;
+            vec[i].iov_len = buffer_rsize(buf);
+            i++;
+            if (i == SM_MAX_IOV) {
+                break;
+            }
+        }
+
+        rv = writev(fd, vec, i);
+        if (rv <= 0) {
+            *err = errno;
+            return rv;
+        }
+
+        total += rv;
+
+        while (chain->rbuf != chain->wbuf) {
+            buf = chain->rbuf;
+            rlen = buffer_rsize(buf);
+            if (rv >= rlen) {
+                rv -= rlen;
+                chain->rbuf = buf->next;
+                buffer_pool_put(pool, buf);
+            } else {
+                buf->pos += rv;
+                rv = 0;
+                break;
+            }
+        }
+
+        buf = chain->wbuf;
+        if (rv > 0) {
+            assert(rv <= buffer_rsize(buf));
+            buf->pos += rv;
+            if (buf->pos == buf->last) {
+                buf->pos = buf->last = buf->start;
+                break;
+            }
+        }
+    }
+
+    return total;
+}
---
+++ kznet/buffer.h
@@ -0,0 +1,54 @@
+#ifndef _KZNET_BUFFER_H_
+#define _KZNET_BUFFER_H_
+
+#include <sys/types.h>
+
+
+typedef struct buffer buffer_t;
+typedef struct buffer_chain buffer_chain_t;
+typedef struct buffer_pool buffer_pool_t;
+
+
+struct buffer {
+    struct buffer *next;
+    unsigned char *pos;
+    unsigned char *last;
+    unsigned char *start;
+    unsigned char *end;
+};
+
+struct buffer_chain {
+    buffer_t *rbuf;
+    buffer_t *wbuf;
+
+    buffer_pool_t *pool;
+};
+
+
+#define SM_MAX_IOV   8
+#define BUFFER_SIZE  4096
+#define BUFFER_HSIZE (sizeof(struct buffer))
+
+
+static inline size_t buffer_rsize(buffer_t *buf)
+{
+    return (size_t)(buf->last - buf->pos);
+}
+
+static inline size_t buffer_wsize(buffer_t *buf)
+{
+    return (size_t)(buf->end - buf->last);
+}
+
+buffer_pool_t *buffer_pool_create(int maxsize);
+
+void buffer_pool_delete(buffer_pool_t *pool);
+
+buffer_t *buffer_pool_get(buffer_pool_t *pool);
+
+int buffer_pool_put(buffer_pool_t *pool, buffer_t *buf);
+
+ssize_t buffer_chain_recv(buffer_chain_t *chain, int fd, int *err);
+ssize_t buffer_chain_send(buffer_chain_t *chain, int fd, int *err);
+
+#endif
